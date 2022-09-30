ec8feb4dd26f557cdfa0230397034c4d5c48e236
albonec <agbonecchi@gmail.com>
2022-09-29 21:57:00 -0700

    basics

--- views.py
+++ views.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@views.route("/views")
 def home():
     return render_template("index.html", name="Alessandro");
+
+@views.route("/views/profile")
+def profile():
+    return render_template("profile.html");
+
+@views.route("/json")
+def getJson():
+    return jsonify({'name': 'vladimir', 'soviet': True})
+
+
+@views.route("/data")
+def getIncomingData():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/views/go-to-homepage")
+def goToHomepage():
+    return redirect(url_for("views.getJson"))
