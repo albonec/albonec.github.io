6ee44bd15cb1da45db2e82a312bca9fe18219fc8
albonec <agbonecchi@gmail.com>
2022-10-07 22:05:26 -0700

    added routing for subpages, no more js && git push

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__' :
-    app.run(debug=True, port=8000);
+    app.run(debug=False, port=8000);
--- views.py
+++ views.py
@@ -4,8 +4,20 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def main():
-    return render_template("index.html");
+    return render_template("index.html")
 
 @views.route("/Home.html")
 def home():
-    return render_template("Home.html");
+    return render_template("Home.html")
+
+@views.route("/Academics.html")
+def academics():
+    return render_template("Academics.html")
+
+@views.route("/Athletics.html")
+def athletics():
+    return render_template("Athletics.html")
+
+@views.route("/Hobbies+Interests.html")
+def hobbies_interests():
+    return render_template("Hobbies+Interests.html")
